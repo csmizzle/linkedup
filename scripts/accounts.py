f0c2a0a05f0fde0fc36753940d2b87fcf6849766
chris <42915698+csmizzle@users.noreply.github.com>
2021-08-17 11:02:31 -0400

    Update accounts.py

--- scripts/accounts.py
+++ scripts/accounts.py
@@ -7,7 +7,7 @@ def get_account() -> list:
     :return: account selection from
     """
     account_list = [
-        ['chrissmith700@gmail.com', 'Hq4_7!lowbar'], # add accounts here for linkedin login
+        # ['chrissmith700@gmail.com', 'Hq4_7!lowbar'], # add accounts here for linkedin login
         # ['jmariegao@gmail.com', "c5@'F>LjMN`^(#<?"]
     ]
     account = random.choice(account_list)
